c145e5f71177f7c6f001373e075ff9d71a5f5d6d
Kim-Dongdong <kdssos11@naver.com>
2026-07-01 23:40:33 +0900

    chore: 프로젝트 초기 세팅

---
+++ src/test/java/com/chwihap/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chwihap.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
