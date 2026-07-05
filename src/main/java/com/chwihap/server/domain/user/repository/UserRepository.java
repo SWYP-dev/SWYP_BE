79a15f3d11c6d89ead331ca620b1d66843b2d0b5
손세영 <187326608+SonSeYoung02@users.noreply.github.com>
2026-07-06 02:44:55 +0900

    feat: 유저 리포지토리 추가
    - UserRepository.java

---
+++ src/main/java/com/chwihap/server/domain/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.chwihap.server.domain.user.repository;
+
+import com.chwihap.server.domain.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
