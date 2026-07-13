48820353cb6383adaac31e7e0275d01ab0f89e7b
손세영 <187326608+SonSeYoung02@users.noreply.github.com>
2026-07-13 14:22:59 +0900

    [Feature] 카드 및 전형 단계 관리 기능 개선 및 예외 처리 추가 (#14)
    * feat: 지원현황 에러코드 추가
    
    - 회사명, 공고명, 공고링크, 전형이름이 조건에 맞는지 검증후 반환하는 에러코드 작성
    
    * feat: 전역 핸들러 추가
    
    - Json 본문을 서버가 읽거나 반환하지 못하는 경우 예외처리 추가
    
    * feat: 카드 생성및 삭제시 JobPosting 연동
    
    - JobPosting에서 originalUrl의 최대길이 2048로 설정
    - JobPostingRepository에서 공고 Id와 유저 아이디를 바탕으로 공고를 찾는 JPA 추가
      - 카드 생성시 JobPosting에 객체 생성(Direct)
      - 카드 삭제시 JobPosting에 객체 삭제(Direct)
    
    * feat: 카드 메모 NotNull 수정
    
    - 사용하지 않음
    
    * feat: 카드 수동 생성 및 업데이트 로직 추가
    
    - [3.3] 사용자가 카드를 수동으로 생성하는 로직(유효성 검사 기준에 맞게 데이터 가공 및 예외처리)
    - [3.4] 사용자가 카드를 수동으로 업데이트 하는 로직(유효성 검사 기준에 맞게 데이터 가공 및 예외처리)
    - 예외처리 메소드 생성
      - validateCompanyName : 회사 이름 검증
      - validateJobPostingName : 공고 이름 검증
      - validateJobPostingLink : 공고 링크 검증
      - validateStageName : 스테이지 이름 검증

--- src/main/java/com/chwihap/server/domain/kanban/dto/KanbanCardRequest.java
+++ src/main/java/com/chwihap/server/domain/kanban/dto/KanbanCardRequest.java
@@ -1,9 +1,11 @@
 package com.chwihap.server.domain.kanban.dto;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 
 public record KanbanCardRequest(
-        @NotNull(message = "postingId는 필수입니다.")
+        @NotNull
+        @Min(1)
         Long postingId
 ) {
 }
