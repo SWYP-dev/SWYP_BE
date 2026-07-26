cbedeeb0d37236534387a06311e1ede83914155e
손세영 <187326608+SonSeYoung02@users.noreply.github.com>
2026-07-26 22:44:30 +0900

    fix: 카드 request 수정
    - name 필드 제거

--- src/main/java/com/chwihap/server/domain/document/dto/DocumentLinkCreateRequest.java
+++ src/main/java/com/chwihap/server/domain/document/dto/DocumentLinkCreateRequest.java
@@ -7,10 +7,6 @@ import jakarta.validation.constraints.Size;
 
 public record DocumentLinkCreateRequest(
         @NotBlank
-        @Size(max = 255)
-        String name,
-
-        @NotBlank
         @Size(max = 500)
         String url,
 
