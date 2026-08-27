d0e6d5e1d43c15693483772e7f14685d33bc8bd9
Kim-Dongdong <kdssos11@naver.com>
2026-08-28 04:08:07 +0900

    feat: Prometheus 메트릭 엔드포인트 추가

--- src/main/java/com/chwihap/server/global/security/SecurityConfig.java
+++ src/main/java/com/chwihap/server/global/security/SecurityConfig.java
@@ -32,7 +32,8 @@ public class SecurityConfig {
             "/swagger-ui/**",
             "/swagger-ui.html",
             "/v3/api-docs/**",
-            "/actuator/health"
+            "/actuator/health",
+            "/actuator/prometheus"
     };
 
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
